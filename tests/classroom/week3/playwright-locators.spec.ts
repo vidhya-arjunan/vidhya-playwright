be33a089390c3ed71a9d7d5b3e6216f959f881a7
Vidhya <vidhya.vijaykumar2811@gmail.com>
2026-04-16 22:29:31 +0530

    login flow for leaftaps

--- tests/classroom/week3/playwright-locators.spec.ts
+++ tests/classroom/week3/playwright-locators.spec.ts
@@ -1,6 +1,10 @@
 import test from "@playwright/test";
 
-test('playwright locators', ({page}) =>{
-    page.goto("https://leaftaps.com/opentaps/control/main")
-    
+test('playwright locators', async ({ page }) => {
+    await page.goto("https://leaftaps.com/opentaps/control/main");
+ 
+    //logging in
+    await page.getByLabel("Username").fill("demosalesmanager");
+    await page.getByLabel("Password").fill("crmsfa");
+    await page.locator(".decorativeSubmit").click();
 });
